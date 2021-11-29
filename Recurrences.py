6692e784c7fc57cfbd2dd0ca9405d813d586a1d0
Jackreece Ejini <jackreeceejini@gmail.com>
2021-11-29 13:27:25 -0500

    added regions in a plane

--- Recurrences.py
+++ Recurrences.py
@@ -16,3 +16,22 @@ def hanoi(n_disks):
 # print("Hanoi 8 ", hanoi(8))
 # print("Hanoi 64", hanoi(64))
 
+
+def regions_in_plane(n_lines):
+    """
+    Maximum number of regions
+    defined by n_lines in a plane
+    Closed Form: regions_in_planes = (n_lines(n_lines + 1) + 1)/2
+    """
+    if n_lines == 0:
+        return 1
+    return regions_in_plane(n_lines - 1) + n_lines
+
+if __name__ == "__main__":
+  print("regions_in_plane: ", regions_in_plane(0))
+  print("regions_in_plane: ", regions_in_plane(1))
+  print("regions_in_plane: ", regions_in_plane(2))
+  print("regions_in_plane: ", regions_in_plane(3))
+  print("regions_in_plane: ", regions_in_plane(4))
+  print("regions_in_plane: ", regions_in_plane(7))
+
